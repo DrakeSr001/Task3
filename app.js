c3a744df706d691766edc93d4cbc4fb081842797
DrakeSr001 <111085072+DrakeSr001@users.noreply.github.com>
2023-03-19 07:44:35 +0200

    2nd commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let title = document.getElementById("title");
+let cost = document.querySelectorAll("#cost input");
+let count = document.getElementById("count");
+let department = document.getElementById("department");
+
+
+
+
+
+cost[0].onkeyup = function(){
+    alert("test");
+}
